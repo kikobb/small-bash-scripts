8d11ce4a6a5d1f7e8225d1989bbf1ef8c90067f3
k <xbarna02@stud.fit.vutbr.cz>
2020-06-29 15:55:15 +0200

    sync_base (some changes/fales were not tracked in repo)

--- init_linux.sh
+++ init_linux.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-############
 # .profile #
-############
+
 #initialize PATH var in .profile 
 cat << EOF >> ~/.profile
 #-------------------#
@@ -15,4 +14,37 @@ fi
 
 # END CUSTOM INIT #
 #-----------------#
+EOF
+
+
+
+
+# .bashrc #
+
+#create custom command line prompt look 
+sed -i '/unset color_prompt force_color_prompt/c\#unset color_prompt force_color_prompt' ~/.bashrc
+cat << EOF >> ~/.bashrc
+#-------------------#
+# BEGIN CUSTOM INIT #
+
+#cusotm command prompt
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u:\[\033[01;34m\]\W\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@:\W\$ '
+fi
+
+#before delete foloving line uncomment same line on line 64
+unset color_prompt force_color_prompt
+
+# END CUSTOM INIT #
+#-----------------#
+
+EOF
+
+#tldr
+cat << EOF >> ~/.bashrc
+#tldr located at ~/bin/small-bash-scripts
+#enabling shell completition
+complete -W "$(tldr 2>/dev/null --list)" tldr
 EOF
